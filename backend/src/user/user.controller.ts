1646567bd01bd8ea2749e4b4c8f4cac0dd696c0a
Daniel Istoc <istoc.daniel26@gmail.com>
2025-01-16 09:34:29 +0100

    fix(user-controller): change endpoint from users/register to /register

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto'; // A DTO importálása
 
-@Controller('users')  // Base route: /users
+@Controller()  // Base route: /users
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('register') // A végpont: POST /users/register
+  @Post('register') // A végpont: POST /register
   async register(@Body() createUserDto: CreateUserDto) {
     // A createUserDto tartalmazza az email és password mezőket
     return this.userService.createUser(createUserDto.email, createUserDto.password);
